575501e3cbc8f39907ba0d907415471255ae5764
shahar-y <shya95@gmail.com>
2021-07-19 21:42:10 +0300

    Initial commit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    bool jumpKeyWasPressed;
+    private float horizontalInput;
+    private Rigidbody rigidBodyComponent;
+    [SerializeField] private Transform groundCheckTransform;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBodyComponent = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            jumpKeyWasPressed = true;
+        }
+
+        horizontalInput = Input.GetAxis("Horizontal");
+    }
+
+    private void FixedUpdate()
+    {
+        if (Physics.OverlapSphere(groundCheckTransform.position, 0.1f).Length == 1)
+        {
+            return;
+        }
+        
+        if (jumpKeyWasPressed)
+        {
+            rigidBodyComponent.AddForce(Vector3.up * 5, ForceMode.VelocityChange);
+            jumpKeyWasPressed = false;
+        }
+
+        rigidBodyComponent.velocity = new Vector3(horizontalInput, rigidBodyComponent.velocity.y, 0);
+
+    }
+
+
+
+}
